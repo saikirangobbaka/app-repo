185f11cf2701c808044e5c05bfcaa1f7a3d2a7b7
saikirangobbaka <159161538+saikirangobbaka@users.noreply.github.com>
2026-06-11 16:50:58 +0530

    Update HelloWorld.java

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,17 @@
+// File: src/main/java/com/example/HelloWorld.java
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from DevOps Project!");
+        System.out.println("Application is running successfully");
+    }
+
+    public static String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
